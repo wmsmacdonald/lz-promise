dd75a977a28517e6a60ae2fb5764d8a0a6f96654
William MacDonald <wmsmacdonald@gmail.com>
2018-07-24 00:40:03 -0700

    added types

--- src/lzPromise.spec.ts
+++ src/lzPromise.spec.ts
@@ -1,11 +1,10 @@
-import { lz } from './lzPromise';
-import * as sinon from 'sinon';
-import { expect } from 'chai';
+import { lz, LazyPromise, InvokableLazyPromise } from "./lzPromise";
+import * as sinon from "sinon";
+import { expect } from "chai";
 
-
-describe('lz', () => {
-  it('should only call once after two evaluations', async () => {
-    const fake = sinon.fake.returns(Promise.resolve('result'));
+describe("lz", () => {
+  it("should only call once after two evaluations", async () => {
+    const fake = sinon.fake.returns(Promise.resolve("result"));
 
     const lazyValue = lz(fake);
 
@@ -15,17 +14,43 @@ describe('lz', () => {
     expect(fake.callCount).to.equal(1);
   });
 
-  describe('then', () => {
-    it('should change the resolved value to value returned in then', async () => {
-      const lazyValue = lz(() => Promise.resolve('result'));
+  describe("then", () => {
+    it("should change the resolved value to value returned in then", async () => {
+      const lazyValue = lz(() => Promise.resolve("result"));
 
       const lazyChangedValue = (lazyValue as any).then(result => {
-        expect(result).to.equal('result');
-        return 'changedResult';
+        expect(result).to.equal("result");
+        return "changedResult";
       });
 
-
-      expect(await lazyChangedValue()).to.equal('changedResult');
+      expect(await lazyChangedValue()).to.equal("changedResult");
     });
+  });
+});
+
+describe("join", () => {
+  it("should call handler with results as arguments", () => {
+    const lazyPromise1: InvokableLazyPromise<number> = lz(() =>
+      Promise.resolve(1)
+    );
+    const lazyPromise2: InvokableLazyPromise<number> = lz(() =>
+      Promise.resolve(2)
+    );
+
+    const lazyJoined: InvokableLazyPromise<[number, number]> = LazyPromise.join(
+      lazyPromise1,
+      lazyPromise2,
+      (value1: number, value2: number) => {
+        expect(value1).to.equal(1);
+        expect(value2).to.equal(2);
+      }
+    );
+
+    lazyJoined.then(([value1, value2]) => {
+      expect(value1).to.equal(1);
+      expect(value2).to.equal(2);
+    });
+
+    LazyPromise.join()
   });
 });
--- src/lzPromise.ts
+++ src/lzPromise.ts
@@ -1,25 +1,66 @@
-export const join = (...args) => {
-  const handler = args[args.length - 1];
-  const lazyAllPromises = lz(() => Promise.all(args.slice(0, -1)));
-  return (lazyAllPromises as any).then(results => handler(...results));
-};
+type GenericHandler = (results: Array<any>) => any | Promise<any>;
 
-export const lz = promiseCreator => {
+export class LazyPromise {
+  static join<R1>(
+    arg1: any | Promise<R1>,
+    handler: (R1) => Promise<any> | any
+  ): InvokableLazyPromise<[R1]>;
+  static join<R1, R2>(
+    arg1: any | Promise<R1>,
+    arg2: any | Promise<R2>,
+    handler: (R1, R2) => Promise<any> | any
+  ): InvokableLazyPromise<[R1, R2]>;
+  static join<R1, R2, R3>(
+    arg1: any | Promise<R1>,
+    arg2: any | Promise<R2>,
+    arg3: any | Promise<R3>,
+    handler: (R1, R2, R3) => Promise<any> | any
+  ): InvokableLazyPromise<[R1, R2, R3]>;
+  static join<R1, R2, R3, R4>(
+    arg1: any | Promise<R1>,
+    arg2: any | Promise<R2>,
+    arg3: any | Promise<R3>,
+    arg4: any | Promise<R4>,
+    handler: (R1, R2, R3, R4) => Promise<any> | any
+  ): InvokableLazyPromise<[R1, R2, R3, R4]>;
+  static join<R1, R2, R3, R4, R5>(
+    arg1: any | Promise<R1>,
+    arg2: any | Promise<R2>,
+    arg3: any | Promise<R3>,
+    arg4: any | Promise<R4>,
+    arg5: any | Promise<R5>,
+    handler: (R1, R2, R3, R4, R5) => Promise<any> | any
+  ): InvokableLazyPromise<[R1, R2, R3, R4, R5]>;
+
+  static join(...args: Array<any | Promise<any> | GenericHandler>) {
+    const handler = args[args.length - 1];
+    const lazyAllPromises = lz(() => Promise.all(args.slice(0, -1)));
+    return lazyAllPromises.then(results => handler(...results));
+  }
+}
+
+export interface InvokableLazyPromise<T> extends Promise<T> {
+  (): Promise<T>;
+}
+
+export function lz<T>(
+  promiseCreator: () => Promise<T>
+): InvokableLazyPromise<T> {
   let promise;
 
-  const evaluate = () => {
+  const evaluate = <InvokableLazyPromise<T>>function() {
     if (!promise) {
       promise = promiseCreator();
     }
     return promise;
   };
 
-  evaluate['then'] = (onFulfilled, onRejected) =>
+  evaluate.then = (onFulfilled, onRejected) =>
     lz(() => evaluate().then(onFulfilled, onRejected));
 
-  evaluate['catch'] = onRejected => lz(() => evaluate().catch(onRejected));
+  evaluate.catch = onRejected => lz(() => evaluate().catch(onRejected));
 
-  evaluate['finally'] = onFinally => lz(() => evaluate().finally(onFinally));
+  evaluate.finally = onFinally => lz(() => evaluate().finally(onFinally));
 
   return evaluate;
-};
+}
